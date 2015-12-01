23313964b75a6d7ea3e26de7bed9700e916a8202
BenjaGross <benja.j.gross@gmail.com>
2015-12-01 14:38:46 -0500

    adds more specs and makes outline for question and category

--- js/models/category.js
+++ js/models/category.js
@@ -1,3 +1,15 @@
 var Category = Backbone.Model.extend({
+  defaults: {
+    questionList: []
+  },
+  initialize: function(data){
+    // Build list of questions and return array
+  },
+  compileList: function(){
+    // takes five questions from category
+  },
+  buildQuestion: function(){
+    adds question to category
+  }
 
 });
--- js/models/question.js
+++ js/models/question.js
@@ -1,4 +1,14 @@
 var Question = Backbone.Model.extend({
-  
-  
+  initialize: function(data){
+    // Set question text, answer, and value
+  },
+  markCorrect: function(){
+    // add score
+  }
+  markIncorrect: function(){
+    // subtract Score
+  }
+
+
+
 })
