1c1d04bc26f611987468af140689442423b14060
BenjaGross <benja.j.gross@gmail.com>
2015-12-15 12:27:51 -0500

    adds event to mainview to get question and set up api call

--- js/views/main_view.js
+++ js/views/main_view.js
@@ -2,10 +2,15 @@ var MainView = Backbone.View.extend({
   initialize : function(){
     this.render();
   },
-
+  events: {
+    "click .button#getQuestion":     "getQuestion"
+  },
   render : function(){
     var html = '<h1>This is the main view yo!</h1>'
     this.$el.append(html);
     return this;
+  },
+  getQuestion : function(){
+    // Calls API and returns a random question
   }
 });
