d9b868468f480e9060dbbeacbc6f4f0777556804
BenjaGross <Benja.J.Gross@gmail.com>
2015-12-25 14:34:24 -0700

    adds api call calss skeleton and test file

--- js/models/api_call.js
+++ js/models/api_call.js
@@ -1,1 +1,7 @@
 // http://jservice.io/api/clues?category=1
+
+var APICall = {
+    APICall.prototype.getCategory = function() {
+        // Gets Categroy from api
+    };
+}
---
+++ spec/javascripts/model_specs/api_call.spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+describe("API Call", function() {
+
+  it("should fail for a test", function() {
+    // Fail!
+  });
+
+});
